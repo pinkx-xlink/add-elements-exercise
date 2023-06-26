3b9b594a33f07bb78fe7bdcb71a613fde85f8dac
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-25 21:49:37 -0400

    Add comments with exercise instructions

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!!!!!';
 
 container.appendChild(content);
+
+
+//<p> with red tag "Hey I'm red!"
+
+
+
+//<h3> with blue text "Hey I'm blue!"
+
+
+// <div> with black border and pink bg color
+    //<h1> "I'm a div"
+    //<p> "ME TOO!!!!!"
+    //HINT: after creating <div> w createElement, append <h1> and <p> to it before adding it to container
+    
